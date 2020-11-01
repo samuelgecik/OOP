54cbb145ac940f92fb995c7370868624eaafcc78
samo <samuel.gecik@student.tuke.sk>
2020-11-01 16:00:10 +0100

    moving partly done

---
+++ Merlin/Actors/CrackedCrystal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Merlin
+{
+    public class CrackedCrystal : Crystal
+    {
+        private int remainingUses;
+
+        public CrackedCrystal(PowerSource source) : this(source, 3)
+        {
+
+        }
+
+        public CrackedCrystal(PowerSource source, int remaingUses) : base(source)
+        {
+            remainingUses = remaingUses;
+        }
+
+        public override void TurnOn()
+        {
+            if (remainingUses-- > 0)
+            {
+                base.TurnOn();
+            }
+        }
+    }
+}
