dd6bd6541067053759783c38093f360caebad383
samo <samuel.gecik@student.tuke.sk>
2020-11-18 00:34:58 +0100

    dc

--- 1st_assignment/Merlin/Actors/IMovable.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace Merlin.Actors
-{
-    public interface IMovable
-    {
-    }
-}
--- 1st_assignment/Merlin/Actors/IObservable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Merlin
-{
-    public interface IObservable
-    {
-        void Subscribe(IObserver observer);
-        void Unsubscribe(IObserver observer);
-    }
-}
